b24113ab242f986e8da09ee335abac69599b3fac
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2015-11-03 20:57:34 -0800

    yet more typos

--- tests/travisCustomize.sh
+++ tests/travisCustomize.sh
@@ -6,8 +6,8 @@
 set -e  # exit on error
 
 # no backups on travis
-cp $GS_HOME/tests/sys/local/client/tode-scripts/* $GS_HOME/sys/local/client/tode-scripts
-cp $GS_HOME/tests/tode/* $GS_HOME/etc/GsDevKit_seaside31/tode
+cp $PROJECT_HOME/tests/sys/local/client/tode-scripts/* $GS_HOME/sys/local/client/tode-scripts
+cp $PROJECT_HOME/tests/tode/* $GS_HOME/etc/GsDevKit_seaside31/tode
 
 # use custom branch of tODE
-cp $GS_HOME/tests/sys/local/gsdevkit_bin/* $GS_HOME/sys/local/gsdevkit_bin
+cp $PROJECT_HOME/tests/sys/local/gsdevkit_bin/* $GS_HOME/sys/local/gsdevkit_bin
