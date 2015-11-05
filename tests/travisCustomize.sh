949beb39799f91440704366506d861124d1b1dec
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2015-11-05 09:50:54 -0800

    add git user info for project new test torun

--- tests/travisCustomize.sh
+++ tests/travisCustomize.sh
@@ -11,3 +11,6 @@ cp $GS_HOME/tests/tode/* $GS_HOME/etc/GsDevKit_seaside31/tode
 
 # use custom branch of tODE
 cp $GS_HOME/tests/sys/local/gsdevkit_bin/* $GS_HOME/sys/local/gsdevkit_bin
+
+git config --global user.email "GsDevKit_seaside@example.com"
+git config --global user.name "GsDevKit seaside"
